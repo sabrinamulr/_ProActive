4f1d768b5c5aad4e14d1b30e94df4e29f11b9c2f
hausbauer66 <hausbauer688@gmail.com>
2026-01-04 00:03:40 +0100

    FeedbackDB

--- Models/Lisa/Antwort.cs
+++ Models/Lisa/Antwort.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
         public int QuestionId { get; set; }
+        public int ProjectId { get; set; }
         public int Rating { get; set; } // 1–5
-        public string? TextAnswer { get; set; } // für offene Fragen
         public DateTime Date { get; set; }
 
     }
